f6ad1fe735fcc0940982d9c0c391aee3f8b14014
A <>
2022-12-01 00:22:58 -0500

    update site copy and include textarea & button

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,44 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Automagic website copy | James Stewart</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Automagically create website copy for your web3 business.</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Add a small sentence about what value your business provides. We do the rest. </h2>
+          </div>
+          <div className="prompt-container">
+            <textarea 
+              className="prompt-box" 
+              placeholder="start typing here... this is the way."
+              value={userInput}
+              onChange={onUserChangedText}
+            />
+            <div className="prompt-buttons">
+              <a className="generate-button" onClick={null}>
+                <div className="generate">
+                  <p>Automagic!</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
       </div>
     </div>
   );
